35396d4d4e4b1750eeec58fb7a343df5f1c57ee7
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-07-12 13:07:19 +0200

    Update hackerman.ps1

--- hackerman.ps1
+++ hackerman.ps1
@@ -1,18 +1,24 @@
 $CAPSABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 $lowabc = "abcdefghijklmnopqrstuvwxyz"
+$numbers = "1234567890"
 $global:STRING = ""
 $global:RANDOM = 1
+$global:SPEED = 2500
 while($true) {
-    $lenght = Get-Random -Minimum 8 -Maximum 25
-    Write-Host $lenght
+    $lenght = Get-Random -Minimum 8 -Maximum 75
+    $global:STRING = ""
     for($i = 0; $i -ne $lenght; $i++ ){
-        $caps = $true, $false | Get-Random
-        $global:STRING = ""
-        $global:RANDOM = Get-Random -Minimum 0 -Maximum 25
-        switch($caps){
-            $true {$global:string = $global:string + $CAPSABC[$global:RANDOM]}
-            $false {$global:string = $global:string + $lowabc[$global:RANDOM]}
+        $type = "uppercase", "lowercase", "number" | Get-Random
+        #$global:RANDOM = Get-Random -Minimum 0 -Maximum 25
+        switch($type){
+            "uppercase" {$global:RANDOM = Get-Random -Minimum 0 -Maximum 25; $global:STRING = $global:STRING + $CAPSABC[$global:RANDOM]}
+            "lowercase" {$global:RANDOM = Get-Random -Minimum 0 -Maximum 25; $global:STRING = $global:STRING + $lowabc[$global:RANDOM]}
+            "number" {$global:RANDOM = Get-Random -Minimum 0 -Maximum 8; $global:STRING = $global:STRING + $numbers[$global:RANDOM]}
         }
-    Write-Host $global:STRING -ForegroundColor Green
     }
+    Start-Sleep -Milliseconds $global:SPEED
+    if($global:SPEED -ne 0){
+    $global:SPEED = $global:SPEED - 100
+    }
+    Write-Host $global:STRING -ForegroundColor Green
 }
