6066148eae6ade8b6a4a896d95bce09148d6be72
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-07-17 18:05:26 +0200

    Update AfterRestartNo.ps1

--- funnyshutdown/AfterRestartNo.ps1
+++ funnyshutdown/AfterRestartNo.ps1
@@ -2,6 +2,7 @@ Add-Type -AssemblyName PresentationCore, PresentationFramework
 $Result = [System.Windows.MessageBox]::Show('Dachtest du wirklich du entkommst mir so einfach?' ,'LOL GET HACKED BOZO' ,'YesNo' ,'Question')
 switch($Result){
     'Yes' {
+        [System.Windows.MessageBox]::Show('Schlechte Idee' ,'Schon dumm von dir...' ,'Ok' ,'Error')
         Get-Process | Where-Object {$_.MainWindowTitle -ne ''} | Stop-Process
         cd Desktop
         $counter = 0
