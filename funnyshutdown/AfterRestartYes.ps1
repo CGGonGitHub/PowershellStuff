c0a2b94b6f4d24b5c46f190b3878c4bd1da98032
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-07-17 18:00:45 +0200

    Update AfterRestartYes.ps1

--- funnyshutdown/AfterRestartYes.ps1
+++ funnyshutdown/AfterRestartYes.ps1
@@ -1,2 +1,3 @@
 Add-Type -AssemblyName PresentationCore, PresentationFramework
 $Result = [System.Windows.MessageBox]::Show('Sorry dafür, war ausversehen' ,'Sorry bruder' ,'Ok' ,'Information')
+Remove-Item Documents\SEXWARE\
