6fcf1276c9a535935ccf56552f2fddafefe0aa03
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-07-17 18:31:50 +0200

    Delete AfterRestartYes.ps1

--- funnyshutdown/AfterRestartYes.ps1
+++
@@ -1,3 +0,0 @@
-Add-Type -AssemblyName PresentationCore, PresentationFramework
-$Result = [System.Windows.MessageBox]::Show('Sorry dafür, war ausversehen' ,'Sorry bruder' ,'Ok' ,'Information')
-Remove-Item Documents\SEXWARE\
