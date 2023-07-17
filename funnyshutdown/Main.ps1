ca6220fba3e2742f757d5379dbb10e9eae23a2f7
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-07-17 17:40:16 +0200

    Create Main.ps1

---
+++ funnyshutdown/Main.ps1
@@ -0,0 +1,11 @@
+Add-Type -AssemblyName PresentationCore, PresentationFramework
+while($true){
+    $Result = [System.Windows.MessageBox]::Show('Du wurdest gehackt! Willst du jetzt runterfahren? >:)' ,'Tschau Kakao' ,'YesNo' ,'Warning')
+    switch($Result){
+        'Yes'{shutdown -t 10 -c '*Böse Lache* Hahahhaha'}
+        'No'{
+            
+            shutdown -t 10 -c 'Mir egal, wird jetzt trotzdem runtergefahren :)'
+            }
+    }
+}
